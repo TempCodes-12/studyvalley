d6cf7ee59a58f289cad6e6de3a76fa7d738480ff
Hein Zarni Maung <tempcodeveloper@gmail.com>
2022-06-22 20:07:24 +0630

    thank you noti

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//hamburger
 const hamburger = document.querySelector('.hamburger');
 const mobile_menu = document.querySelector('.mobile-nav');
 
@@ -12,17 +13,32 @@ document.querySelectorAll('.mobo-nav-link').forEach(n => n.
         mobile_menu.classList.remove('is-active');
     }))
 
+//form
 const btn = document.querySelector('.btn');
 const formcontainer = document.querySelector('.formcontainer')
 
 btn.addEventListener('click', function(){
     btn.classList.toggle('is-active');
     formcontainer.classList.toggle('is-active');
+    
 })
+
 
 document.querySelectorAll('.close').forEach(n => n.
     addEventListener("click",() =>{
         btn.classList.remove('is-active');
         formcontainer.classList.remove('is-active');
     }))
+    
+ //thank you message
+ 
+ const thankyou = document.querySelector('.thankyou')
 
+ function popup () {
+    return formcontainer.classList.remove('is-active'),
+    thankyou.classList.toggle('is-active');
+ }
+
+document.querySelectorAll('.tyclose').forEach(n => n.addEventListener("click",() =>{
+    thankyou.classList.remove('is-active');
+}))
